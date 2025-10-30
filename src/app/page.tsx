7a11d9a74bc58c107988cb12f502166ed19677cb
Jupiter Bania <jupiterbania472@gmail.com>
2025-10-30 02:47:32 +0000

    Single image he dikhna chyea gril pe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
                   </Select>
                </div>
             </div>
-            <div className="grid grid-cols-2 gap-4">
+            <div className="grid grid-cols-1 gap-4">
               {allPhotos.map((photo) => (
                 <ImageCard key={photo.id} photo={photo} />
               ))}
