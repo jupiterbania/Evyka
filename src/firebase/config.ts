6eebd15e9d7116d8f5591d9012cbb46cac95a266
Jupiter Bania <jupiterbania472@gmail.com>
2025-10-30 03:11:06 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7639753836-a5d7c",
+  "appId": "1:1077626701111:web:b37521c949c838e5d646b2",
+  "apiKey": "AIzaSyA5JJ8x9DqXhdIOKB9gbv-Y0zgsYSoFThw",
+  "authDomain": "studio-7639753836-a5d7c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1077626701111"
+};
