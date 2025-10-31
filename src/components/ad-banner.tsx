5c866ca916b392ae909a46582a7a30bbe135a4be
Jupiter Bania <jupiterbania472@gmail.com>
2025-10-31 02:55:44 +0000

    Native Banner can be placed anywhere in the page body. <script async="a

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -4,27 +4,14 @@ import Script from 'next/script';
 export function AdBanner() {
   return (
     <div className="flex justify-center items-center aspect-[3/4] w-full">
-        <div className="flex justify-center items-center w-[300px] h-[250px]">
-            <Script
-                id="adsterra-banner-300-250"
+        <div className="flex flex-col justify-center items-center w-full">
+             <Script 
+                async={true}
+                data-cfasync="false" 
+                src="//pl27958407.effectivegatecpm.com/5f4d99dc5499dc62c5353fdfdfe2e35f/invoke.js"
                 strategy="afterInteractive"
-                dangerouslySetInnerHTML={{
-                __html: `
-                    atOptions = {
-                        'key' : '1aa94d7450572033bf4e3ce4bf8efaa7',
-                        'format' : 'iframe',
-                        'height' : 250,
-                        'width' : 300,
-                        'params' : {}
-                    };
-                `,
-                }}
-            />
-            <Script
-                async
-                src="//www.highperformanceformat.com/1aa94d7450572033bf4e3ce4bf8efaa7/invoke.js"
-                strategy="afterInteractive"
-            />
+             />
+            <div id="container-5f4d99dc5499dc62c5353fdfdfe2e35f"></div>
         </div>
     </div>
   );
