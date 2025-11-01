dc6abd003f0a3f4eaa4648d5e071df952c6c4326
Jupiter Bania <jupiterbania472@gmail.com>
2025-11-01 14:59:53 +0000

    20:23:12.899 Running build in Washington, D.C., USA (East) – iad1 20:23:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 
-
 'use client';
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
@@ -534,11 +533,4 @@ export default function Home() {
       </AlertDialog>
     </div>
   );
-
-    
-
-    
-
-
-
-
+}
