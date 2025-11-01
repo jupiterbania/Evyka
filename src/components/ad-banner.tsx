1e91cbe780714420657e2e97329d15ac4ee0875a
Jupiter Bania <jupiterbania472@gmail.com>
2025-11-01 17:43:41 +0000

    1 he card pe show kr rha hai ads ar 3 pe nhi kr rha hai udhr v lagou ads

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -3,11 +3,42 @@ import { useUser } from '@/firebase';
 import { Card } from './ui/card';
 import { cn } from '@/lib/utils';
 import Script from 'next/script';
+import { useEffect, useId } from 'react';
 
 export function AdBanner() {
   const { user } = useUser();
   const designatedAdminEmail = 'jupiterbania472@gmail.com';
   const isAdmin = user?.email === designatedAdminEmail;
+  const uniqueId = useId().replace(/:/g, ''); // Create a unique ID for each ad instance
+
+  useEffect(() => {
+    if (isAdmin) return;
+    
+    // Ad invocation options
+    const atOptions = {
+        'key' : '5f4d99dc5499dc62c5353fdfdfe2e35f',
+        'format' : 'iframe',
+        'height' : 300,
+        'width' : 160,
+        'params' : {}
+    };
+
+    try {
+        const container = document.getElementById(`container-${uniqueId}`);
+        if (container && container.children.length === 0) { // Only add script if container is empty
+            const script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.innerHTML = `
+                atOptions = ${JSON.stringify(atOptions)};
+                document.write('<scr' + 'ipt type="text/javascript" src="//www.effectivecreativeformat.com/5f4d99dc5499dc62c5353fdfdfe2e35f/invoke.js"></scr' + 'ipt>');
+            `;
+            container.appendChild(script);
+        }
+    } catch (e) {
+        console.error('Ad script error:', e);
+    }
+  }, [uniqueId, isAdmin]);
+
 
   if (isAdmin) {
     return null; // Don't render ad for admin
@@ -22,32 +53,8 @@ export function AdBanner() {
         style={{ animationDelay: '200ms' }}
     >
         <div className="flex flex-col justify-center items-center w-full h-full">
-            <Script 
-                id="ad-banner-script"
-                strategy="afterInteractive"
-            >
-                {`
-                    atOptions = {
-                        'key' : '5f4d99dc5499dc62c5353fdfdfe2e35f',
-                        'format' : 'iframe',
-                        'height' : 300,
-                        'width' : 160,
-                        'params' : {}
-                    };
-                    try {
-                        const container = document.getElementById('container-5f4d99dc5499dc62c5353fdfdfe2e35f');
-                        if (container) {
-                            const script = document.createElement('script');
-                            script.async = true;
-                            script.src = '//www.effectivecreativeformat.com/5f4d99dc5499dc62c5353fdfdfe2e35f/invoke.js';
-                            container.appendChild(script);
-                        }
-                    } catch (e) {
-                        console.error('Ad script error:', e);
-                    }
-                `}
-            </Script>
-            <div id="container-5f4d99dc5499dc62c5353fdfdfe2e35f"></div>
+            {/* The unique ID is assigned here */}
+            <div id={`container-${uniqueId}`}></div>
         </div>
     </Card>
   );
