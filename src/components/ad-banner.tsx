66229c6f22f5baf06ee3bb2cef6825b91fd2a279
Jupiter Bania <jupiterbania472@gmail.com>
2025-11-01 17:28:02 +0000

    Yh remove krdo - <script type="text/javascript"> 	atOptions = { 		'key'

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -1,6 +1,5 @@
 'use client';
 import { useUser } from '@/firebase';
-import Script from 'next/script';
 
 export function AdBanner() {
   const { user } = useUser();
@@ -14,13 +13,7 @@ export function AdBanner() {
   return (
     <div className="flex justify-center items-center aspect-[3/4] w-full">
         <div className="flex flex-col justify-center items-center w-full">
-             <Script 
-                async={true}
-                data-cfasync="false" 
-                src="//pl27958407.effectivegatecpm.com/5f4d99dc5499dc62c5353fdfdfe2e35f/invoke.js"
-                strategy="afterInteractive"
-             />
-            <div id="container-5f4d99dc5499dc62c5353fdfdfe2e35f"></div>
+             {/* The ad script has been removed as requested. */}
         </div>
     </div>
   );
