10b902d1f9db76ead0bc9c89dc01c7ec621bb23f
Jupiter Bania <jupiterbania472@gmail.com>
2025-11-03 12:33:33 +0000

    update - // Import the functions you need from the SDKs you need import

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDL-m7aXQkSzzmK9_5SNAERpvosv-JE3cA",
   authDomain: "focusfinds2-14357216-fd6e9.firebaseapp.com",
   projectId: "focusfinds2-14357216-fd6e9",
-  storageBucket: "focusfinds2-14357216-fd6e9.appspot.com",
+  storageBucket: "focusfinds2-14357216-fd6e9.firebasestorage.app",
   messagingSenderId: "456259078791",
   appId: "1:456259078791:web:4e7abcbd0627d0d7598622"
 };
